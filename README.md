3426a4da92ef5b45e949ac2a50e6ef2bae4038f0
Shubham <shubham11729@gmail.com>
2023-03-30 16:27:11 -0500

    Completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Cookie-Maker
 
-Click [here](https://iamshubhamhere.github.io/Rapid-fire_Game/) to check the Cookie-Maker
+Click [here](https://iamshubhamhere.github.io/Cookies-maker/) to check the Cookie-Maker
--- assets/script/index.js
+++ assets/script/index.js
@@ -4,27 +4,131 @@ const content = document.querySelector('.modal-content');
 const accept = document.querySelector('.accept');
 const settings = document.querySelector('.settings')
 const settingsModal = document.querySelector('.settingsModal');
-
-function openSettings() {
-  settingsModal.style.display = 'inline-block';
-  content.style.display = 'none';
-}
-
-
-
+const prefrences = document.querySelector('.save');
+const browser = document.querySelector('.browser');
+const operatingSystem = document.querySelector('.os');
+const screenWidth = document.querySelector('.sw');
+const screenHeight = document.querySelector('.sh');
 function setCookie(name, value, options = {}) {
   options = {
     path: '/',
-    SameSite: 'Lax',
-    ...options
+    sameSite: 'Lax',
+    ...options,
   };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += `; ${optionKey}`;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += `=${optionValue}`;
+    }
+  }
+
+  document.cookie = updatedCookie;
 }
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
 
-// When the user clicks anywhere outside of the modal, function will close modal
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// function deleteCookie(name) {
+//   setCookie(name, '', {'max-age': -1});
+// }
+
+
+function getScreenWidth() {
+  return screen.width;
+};
+
+function getScreenHeight() {
+  return screen.height;
+};
+
+
+function getBrowser() {
+  if (navigator.userAgent.indexOf('Edg') != -1) {
+    return 'Edge';
+  } if (navigator.userAgent.indexOf('Chrome') != -1) {
+    return 'Chrome';
+  } if (navigator.userAgent.indexOf('Firefox') != -1) {
+    return 'Firefox';
+  } if (navigator.userAgent.indexOf('Safari') != -1) {
+    return 'Safari';
   }
 }
 
+function getOS() {
+  if (navigator.userAgent.indexOf('Windows NT 10.0') != -1) {
+    return 'Windows 10';
+  } if (navigator.userAgent.indexOf('Windows NT 6.3') != -1) {
+    return 'Windows 8.1';
+  } 
+   if (navigator.userAgent.indexOf('Linux') != -1) {
+    return 'Linux';
+  } if (navigator.userAgent.indexOf('Mac') != -1) {
+    return 'Mac/iOS';
+  } else {
+    return 'Unknown';
+  }
+};
+
+function deleteCookie(name) {
+  setCookie(name, '', {'max-age': -1});
+}
+
+  console.log(document.cookie ? 'Cookies available' : 'No cookies found');
+  if (!getCookie('modalShown')) {
+    setTimeout(function () {
+      modal.style.display = 'block';
+    }, 1000);
+  }
+
+accept.addEventListener('click', function(){
+  setCookie('browser', getBrowser(), { maxAge: 15 });
+  setCookie('operatingSystem', getOS(), { maxAge: 15 });
+  setCookie('screen-width', getScreenWidth(), { maxAge: 15 });
+  setCookie('screen-height', getScreenHeight(), { maxAge: 15 });
+  setCookie('modalShown', 'true', { maxAge: 15 });
+  
+  console.log(getCookie('browser'));
+  console.log(getCookie('operatingSystem'));
+  console.log(getCookie('screen-width'));
+  console.log(getCookie('screen-height'));
+  console.log(document.cookie);
+  modal.style.display = "none";
+});
+
+settings.addEventListener('click', function(){
+  settingsModal.style.display = 'inline-block';
+  content.style.display = 'none';
+});
+
+prefrences.addEventListener('click', function(){
+  if (browser.checked === true) {
+    setCookie('browser', getBrowser(), {maxAge: 15});
+    console.log(getCookie('browser'));
+  } if (operatingSystem.checked === true) {
+    setCookie('os', getOS(), {maxAge: 15});
+    console.log(getCookie('os'));
+  } if (screenWidth.checked === true) {
+    setCookie('screen-width', getScreenWidth(), {maxAge: 15});
+    console.log(getCookie('screen-width'));
+  } if (screenHeight.checked === true) {
+    setCookie('screen-height', getScreenHeight(), {maxAge: 15});
+    console.log(getCookie('screen-height'));
+  }
+  console.log(document.cookie);
+  settingsModal.style.display = 'none';
+  content.style.display = 'none';
+  modal.style.display = "none";
+});
